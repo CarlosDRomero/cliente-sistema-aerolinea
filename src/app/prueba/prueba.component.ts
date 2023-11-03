e7aa0957cc63eda1dde5a58d197dbb739ab53428
CarlosC <Carlos.manuelct17@gmail.com>
2023-11-03 15:38:03 -0500

    cambios de prueba

---
+++ src/app/prueba/prueba.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NgModel } from '@angular/forms';
+
+@Component({
+  selector: 'app-prueba',
+  templateUrl: './prueba.component.html',
+  styleUrls: ['./prueba.component.css']
+})
+export class PruebaComponent {
+values: string[] | undefined;
+}
