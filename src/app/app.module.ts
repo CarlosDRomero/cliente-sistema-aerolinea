1664581586f67fab025ca32c7ff5f91635f84db9
Carlos Romero <c6099020@gmail.com>
2023-11-03 16:39:23 -0500

    Reseteo de las pruebas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PruebaComponent } from './prueba/prueba.component';
-import { ChipsModule } from 'primeng/chips';
-import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
-    AppComponent,
-    PruebaComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    ChipsModule,
-    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
